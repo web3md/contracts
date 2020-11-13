51704b9978a198dbaa2d8130995315a08e97d6d5
Team LevX <team@levx.app>
2020-11-14 02:59:45 +0900

    v0.2.0

--- contracts/Blog.sol
+++ contracts/Blog.sol
@@ -8,7 +8,7 @@ import "./libraries/Bytes32Pagination.sol";
 contract Blog {
     using Bytes32Pagination for bytes32[];
 
-    event Created(bytes32 hash);
+    event Created(bytes32 hash, bytes32 parentHash);
     event Updated(bytes32 hash);
     event Liked(bytes32 hash, address liker);
 
@@ -22,12 +22,62 @@ contract Blog {
         bytes32 parentHash;
         address author;
         Revision[] revisions;
-        address[] likes;
+        bytes32[] commentHashes;
+        address[] likers;
         uint256 updatedAt;
     }
 
     mapping(address => bytes32[]) internal _hashesOfAuthor;
-    mapping(bytes32 => Post) public postOfHash;
+    mapping(bytes32 => Post) internal _postOfHash;
+
+    function getPost(bytes32 hash)
+        public
+        view
+        returns (
+            address author,
+            string memory title,
+            string memory body,
+            uint256 updatedAt
+        )
+    {
+        Post storage post = _postOfHash[hash];
+        if (post.author == address(0)) {
+            return (address(0), "", "", 0);
+        } else {
+            Revision storage latest = post.revisions[post.revisions.length - 1];
+            return (post.author, latest.title, latest.body, post.updatedAt);
+        }
+    }
+
+    function numberOfRevisions(bytes32 hash) public view returns (uint256) {
+        Post storage post = _postOfHash[hash];
+        return post.revisions.length;
+    }
+
+    function revisionAt(bytes32 hash, uint256 index) public view returns (Revision memory) {
+        Post storage post = _postOfHash[hash];
+        return post.revisions[index];
+    }
+
+    function numberOfCommentHashes(bytes32 hash) public view returns (uint256) {
+        Post storage post = _postOfHash[hash];
+        return post.commentHashes.length;
+    }
+
+    function commentHashAt(bytes32 hash, uint256 index) public view returns (bytes32) {
+        Post storage post = _postOfHash[hash];
+        return post.commentHashes[index];
+    }
+
+    function numberOfLikers(bytes32 hash) public view returns (uint256) {
+        Post storage post = _postOfHash[hash];
+        return post.likers.length;
+    }
+
+    function likerAt(bytes32 hash, uint256 index) public view returns (address) {
+        Post storage post = _postOfHash[hash];
+        return post.likers[index];
+    }
 
     function numberOfHashesOfAuthor(address author) public view returns (uint256) {
         return _hashesOfAuthor[author].length;
@@ -48,12 +98,11 @@ contract Blog {
     ) public {
         require(bytes(title).length > 0, "empty-title");
         require(bytes(body).length > 0, "empty-body");
-        if (parentHash != bytes32(0)) {
-            require(postOfHash[parentHash].author != address(0), "invalid-parent-hash");
-        }
 
-        bytes32 hash = _hash(msg.sender, title, body, parentHash);
-        Post storage post = postOfHash[hash];
+        bytes32 hash =
+            keccak256(abi.encodePacked(msg.sender, title, body, parentHash, block.number));
+
+        Post storage post = _postOfHash[hash];
         require(post.author == address(0), "already-posted");
         post.author = msg.sender;
         post.updatedAt = block.timestamp;
@@ -63,16 +112,13 @@ contract Blog {
         revision.body = body;
         revision.createdAt = block.timestamp;
 
-        emit Created(hash);
-    }
+        if (parentHash != bytes32(0)) {
+            Post storage parentPost = _postOfHash[parentHash];
+            require(parentPost.author != address(0), "invalid-parent-hash");
+            parentPost.commentHashes.push(hash);
+        }
 
-    function _hash(
-        address author,
-        string memory title,
-        string memory body,
-        bytes32 parentHash
-    ) internal view returns (bytes32) {
-        return keccak256(abi.encodePacked(author, title, body, parentHash, block.number));
+        emit Created(hash, parentHash);
     }
 
     function update(
@@ -83,7 +129,7 @@ contract Blog {
         require(bytes(title).length > 0, "empty-title");
         require(bytes(body).length > 0, "empty-body");
 
-        Post storage post = postOfHash[hash];
+        Post storage post = _postOfHash[hash];
         require(post.author == msg.sender, "not-author");
         post.updatedAt = block.timestamp;
 
@@ -96,15 +142,15 @@ contract Blog {
     }
 
     function like(bytes32 hash) public {
-        Post storage post = postOfHash[hash];
+        Post storage post = _postOfHash[hash];
         require(post.author != address(0), "wrong-hash");
 
-        for (uint256 i = 0; i < post.likes.length; i++) {
-            if (post.likes[i] == msg.sender) {
+        for (uint256 i = 0; i < post.likers.length; i++) {
+            if (post.likers[i] == msg.sender) {
                 revert("already-liked");
             }
         }
-        post.likes.push(msg.sender);
+        post.likers.push(msg.sender);
 
         emit Liked(hash, msg.sender);
     }
